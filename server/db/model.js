63af4a2534314d46904076fe12b3ac0301906df0
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-16 23:08:37 -0700

    Refactor function and improve promises but looping is bad

--- server/db/model.js
+++ server/db/model.js
@@ -1,5 +1,43 @@
 const db = require('./config');
 
+const tests = {
+  fetchBatches(res, loc, moreToFind) {
+    let offset = 0;
+    const batchSize = 3;
+    const keepLooking = moreToFind || true;
+    if (keepLooking === false) {
+      return;
+    }
+
+    db.News.findAll({
+      where: {
+        queryLoc: loc
+      },
+      offset,
+      limit: batchSize
+    })
+    .then((data) => {
+      // send data to socket
+      offset += batchSize;
+      console.log('Batch found 0 -', offset);
+      if (data.length > 0) {
+        return true;
+      }
+      return false;
+    })
+    .then((next) => {
+      if (!next) {
+        console.log(res);
+      } else {
+        this.fetchBatches(loc, true);
+      }
+    })
+    .catch((err) => {
+      console.log('Error with batch find: ', err);
+    });
+  }
+};
+
 module.exports = {
     // all methods return a promise
     // getters resolve with -> [{...}, {...}, ...]
@@ -12,36 +50,6 @@ module.exports = {
         });
     },
 
-    fetchBatches(loc, moreToFind) {
-      let offset = 0;
-      let batchSize = 10;
-      const keepLooking = moreToFind || true;
-      if (keepLooking = false) {
-        return;
-      }
-
-      db.News.findAll({
-        where: {
-          queryLoc: loc
-        },
-        offset: offset,
-        limit: batchSize
-      })
-      .then((data) => {
-        // send data to socket
-        offset += 10;
-        batchSize += 10;
-        if (data.length > 0) {
-          this.fetchBatches(loc, true);
-        } else {
-          this.fetchBatches(loc, false);
-        }
-        return;
-      })
-      .catch((err) => {
-        console.log('Error with batch find: ', err);
-      });
-    },
     // getByTitle(title) {
     //   return db('news').where('title', title)
     //   .catch(err => console.log(`Error getting record by title ${err}`));
@@ -49,15 +57,9 @@ module.exports = {
 
     // Build test queries here:
     test(req, res, next) {
-      const geo = req.body;
-      // loc should be object {lat, lng, rad}
-      console.log('loc object recevied by testGetByLocation: ', geo);
-      const getByLocation = (loc) => {
-        console.log(db.sequelize.fn(`*, (3959 * acos(cos(radians(${loc.lat})) * cos(radians(lat)) * cos(radians(lng) - radians(${loc.lng})) + sin(radians(${loc.lat})) * sin(radians(lat)))) as distance`));
-      };
-      getByLocation(geo)
-        .then((results) => console.log('Results of getByLocation: ', results))
-        .catch((err) => res.send('Error getting vals: ', err));
+      const city = req.body.city;
+      console.log('Testing batch find with city: ', city);
+      tests.fetchBatches(res, city);
     },
 
     getByLocation(loc) {
